3422ec6e90e8857cc25b4a6f5eb33ad013301ec0
SomethingTerrible <walexeymineev@gmail.com>
2021-05-05 20:28:21 +0500

    Добавлены DTO объекты уровня BLL. Изменены поля класса ProductInWarehouses

---
+++ WarehouseManagementSoftware.BLL/DataTransferObject/ProductDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WarehouseManagementSoftware.BLL.DataTransferObject
+{
+    public class ProductDTO
+    {
+        public int Id { get; set; }
+
+        public string  ProductName { get; set; }
+
+    }
+}
